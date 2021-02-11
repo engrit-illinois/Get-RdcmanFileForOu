ebaa115ef2bad186f394063890168744ab855259
mmseng <mmseng@users.noreply.github.com>
2021-02-10 20:52:12 -0600

    updates

---
+++ Get-RdcmanFileForOu.psm1
@@ -0,0 +1,245 @@
+# By mseng3
+function Get-RdcmanFileForLabs {
+	param(
+		[Parameter(Position=0,Mandatory=$true)]
+		[string]$OUDN,
+		
+		[Parameter(Position=1,Mandatory=$true)]
+		[string]$OutputFilePath,
+		
+		[ValidateSet("FlatMachines","SmartGroupsFlat","SmartGroupsGrouped","MirrorOuStructure")]
+		[string]$OutputFormat = "MirrorOuStructure",
+		
+		[int]$MinLabSizeForSmartGroups = 2,
+		
+		[string]$IndentChar = "	"
+	)
+	
+	function Export-File($ou, $comps, $labs) {
+		
+		$name = "$ou OU"
+		
+		# Write header
+		Export "<?xml version=`"1.0`" encoding=`"utf-8`"?>" 0 $false
+		Export "<RDCMan programVersion=`"2.7`" schemaVersion=`"3`">"
+		Export "<file>" 1
+		Export "<credentialsProfiles />" 2
+		Export "<properties>" 2
+		Export "<expanded>True</expanded>" 3
+		Export "<name>$name</name>" 3
+		Export "</properties>" 2
+		
+		# Write a group containing smart groups for each lab
+		switch($OutputFormat) {
+			"FlatMachines" {
+				Export-Comps "all" $ou $comps 2
+			}
+			"SmartGroupsFlat" {
+				Export-SmartGroups $labs 2
+				Export-Comps "all" $ou $comps 2
+			}
+			"SmartGroupsGrouped" {
+				Export-LabGroups $labs $comps
+			}
+			"MirrorOuStructure" {
+				Export-OuGroups 2
+			}
+		}
+		
+		# Write footer
+		Export "</file>" 1
+		Export "<connected />" 1
+		Export "<favorites />" 1
+		Export "<recentlyUsed />" 1
+		Export "</RDCMan>"
+	}
+	
+	function Export-OuChildren($object, $indent) {
+		foreach($child in $object.Children) {
+			$indent0 = $indent + 1
+			$indent1 = $indent + 2
+			$indent2 = $indent + 3
+			
+			$name = $child.OU.Name
+			
+			Export "<group>" $indent0
+			Export "<properties>" $indent1
+			Export "<expanded>False</expanded>" $indent2
+			Export "<name>$name</name>" $indent2
+			Export "<comment>Child OUs of $name OU</comment>" $indent2
+			Export "</properties>" $indent1
+			
+			Export-Comps "structure" $name $child.Computers $indent1
+			Export-Children $child $indent1
+			
+			Export "</group>" $indent0
+		}
+	}
+	
+	function Export-OuGroups($indent) {
+		# Check if custom Get-ADOUStructureObject module is imported
+		if((Get-Module "Get-ADOUStructureObject") -eq $null) {
+			throw "Get-ADOUStructureObject module not imported!"
+		}
+		else {
+			$object = Get-ADOUStructureObject -OUDN $OUDN -Silent -OutputObject
+			if(!$object) {
+				throw "No object received from Get-ADOUStructureObject!"
+			}
+			else {
+				Export-OuChildren $object ($indent + 1)
+			}
+		}
+	}
+	
+	function Export-LabGroups($labs,$comps) {
+		
+		# Start writing a group to contain all lab groups
+		Export "<group>" 2
+		Export "<properties><expanded>True</expanded><name>LabGroups</name></properties>" 3
+		
+		$labGroups = Get-LabGroups $labs
+		foreach($labGroup in $labGroups) {
+			Export "<group>" 3
+			Export "<properties><expanded>False</expanded><name>$labGroup</name></properties>" 4
+			
+			$labGroupLabs = $labs | Where { $_ -like "$labGroup-*" } | Sort
+			Export-SmartGroups $labGroupLabs 4
+			
+			$labComps = $comps | Where { $_ -like "$labGroup-*" } | Sort Name
+			Export-Comps "lab" $labGroup $labComps 4
+			
+			Export "</group>" 3
+		}
+		# Write the end of the group
+		Export "</group>" 2
+	}
+	
+	function Export-SmartGroups($labs,$indent) {
+		$indent1 = $indent + 1
+		
+		# For each lab
+		foreach($lab in @($labs)) {
+			# write a smart group for the lab
+			Export "<smartGroup>" $indent
+			Export "<properties><expanded>False</expanded><name>$lab</name></properties>" $indent1
+			Export "<ruleGroup operator=`"All`"><rule><property>DisplayName</property><operator>Matches</operator><value>$lab</value></rule></ruleGroup>" $indent1
+			Export "</smartGroup>" $indent
+		}
+	}
+	
+	function Export-Comps($type,$group,$comps,$indent) {
+		$indent1 = $indent + 1
+		
+		switch($type) {
+			"all" {
+				$groupName = "All $group OU machines"
+			}
+			"lab" {
+				$groupName = "All $group machines"
+			}
+			"structure" {
+				$groupName = "Computers in $group OU"
+			}
+		}
+		
+		# Start writing a group to contain all comps
+		Export "<group><properties><expanded>False</expanded><name>$groupName</name></properties>" $indent
+		
+		# For each comp
+		foreach($comp in $comps) {
+			# Write it to the file in the group
+			Export "<server><properties><name>$comp</name></properties></server>" $indent1
+		}
+		
+		# Write the end of the group
+		Export "</group>" $indent
+	}
+	
+	function Export($string, $indentSize=0, $append=$true) {
+		# Ensure output file exists, if not create it
+		if(!(Test-Path -PathType leaf -Path $OutputFilePath)) {
+			New-Item -ItemType File -Force -Path $OutputFilePath | Out-Null
+		}
+		
+		$indent = ""
+		for($i = 0; $i -lt $indentSize; $i += 1) {
+			$indent = "$indent$IndentChar"
+		}
+		$string = "$indent$string"
+		
+		if($append) {
+			$string | Out-File $OutputFilePath -Encoding ascii -Append
+		}
+		else {
+			$string | Out-File $OutputFilePath -Encoding ascii
+		}
+	}
+	
+	function Get-LabGroups($labs) {
+		$labGroups = @()
+		
+		foreach($lab in $labs) {
+			$parts = $lab.Split("-")
+			$labGroup = $parts[0]
+			$labGroups += @($labGroup)
+		}
+		
+		# Remove duplicates
+		$labGroups = $labGroups | Select -Unique | Sort
+				
+		$labGroups
+	}
+	
+	function Get-Labs($comps) {
+		$labs = @()
+		
+		# Only create a lab for computers that match a standard naming format
+		# e.g. "<alphanumeric string>-<alphanumeric string>-<alphanumeric string>"
+		$regex = "^[A-Z0-9].*-[A-Z0-9].*-[A-Z0-9].*$"
+		foreach($comp in $comps) {
+			if($comp -match $regex) {
+				$parts = $comp.Split("-")
+				$lab = "$($parts[0])-$($parts[1])"
+				$labs += @($lab)
+			}
+		}
+		
+		# Remove duplicates
+		$labs = $labs | Select -Unique | Sort
+		
+		# Filter out labs that are smaller than the given size
+		# So we don't have a bunch of smart groups for one-off machines
+		$filteredLabs = $labs
+		foreach($lab in $labs) {
+			$compsInLab = $comps | Where { $_ -like "$($lab)*" }
+			$count = @($compsInLab).count
+			if($count -lt $MinLabSizeForSmartGroups) {
+				$filteredLabs = $filteredLabs | Where { $_ -ne $lab }
+			}
+		}
+		
+		$filteredLabs
+	}
+	
+	function Do-Stuff {
+		# Get name of given OUDN
+		$ou = (Get-ADOrganizationalUnit -Filter "DistinguishedName -eq '$OUDN'" | Select Name).Name
+		
+		# Get all comps in OU
+		$comps = (Get-ADComputer -Filter "*" -SearchBase $OUDN | Select Name).Name
+		$compsUpper = @()
+		foreach($comp in $comps) {
+			$compsUpper += @($comp.ToUpper())
+		}
+		$comps = $compsUpper
+		
+		# Get labs based on computer names
+		$labs = Get-Labs $comps
+		
+		# Create RDG file
+		Export-File $ou $comps $labs
+	}
+	
+	Do-Stuff
+}
--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# Get-RdcmanFileForOu
+# Summary
 Outputs an RDG file (for use with RDCMan) with a group structure mimicking the OU structure in a given AD OU.
+
+Optionally organizes groups or smart groups differently depending on the parameters specified.  
+
+# Usage
+1. Download `Get-RdcmanFileForOu.psm1`.
+2. Download dependency `Get-ADOUStructureObject.psm1` from [here](https://github.com/engrit-illinois/Get-RdcmanFileForOu).
+3. Import both files as modules:
+  - ```
+Import-Module "c:\path\to\Get-RdcmanFileForOu.psm1"
+Import-Module "c:\path\to\Get-ADOUStructureObject.psm1"
+```
+4. Run it using the examples provided below.
+
+# Examples
+- A single group containing all machines in a flat structure:
+  - `Get-RdcmanFileForLabs -OUDN "OU=Instructional,OU=Desktops,OU=Engineering,OU=Urbana,DC=ad,DC=uillinois,DC=edu" -OutputFilePath "C:\ews-rdcman-file.rdg" -OutputFormat "FlatMachines" -MinLabSizeForSmartGroups 3`
+- A single group containing all machines in a flat structure, and adjacent to that group, a flat list of smart groups, which search for machines named like `<string>-<string>-*`:
+  - `Get-RdcmanFileForLabs -OUDN "OU=Instructional,OU=Desktops,OU=Engineering,OU=Urbana,DC=ad,DC=uillinois,DC=edu" -OutputFilePath "C:\ews-rdcman-file.rdg" -OutputFormat "SmartGroupsFlat" -MinLabSizeForSmartGroups 3`
+- Smart groups which search for machines named like `<string>-<string>-*`, grouped into groups which contain smart groups named like `<string>-*`. Each group named like `<string>` contains a single group with all computers named like `<string>-*-*`:
+  - `Get-RdcmanFileForLabs -OUDN "OU=Instructional,OU=Desktops,OU=Engineering,OU=Urbana,DC=ad,DC=uillinois,DC=edu" -OutputFilePath "C:\ews-rdcman-file.rdg" -OutputFormat "SmartGroupsGrouped"`
+- COMING SOON: Regular groups and computers in a structure mirroring that of the given OU:
+  - `Get-RdcmanFileForLabs -OUDN "OU=Instructional,OU=Desktops,OU=Engineering,OU=Urbana,DC=ad,DC=uillinois,DC=edu" -OutputFilePath "C:\ews-rdcman-file.rdg" -OutputFormat "MirrorOuStructure"`
+
+# Parameters
+
+### -OUDN \<string\>
+Required string.  
+The DistringuishedName of the OU containing the computers you want in the file.  
+The switch itself can be ommitted if this is the first parameter given.  
+
+### -OutputFile \<string\>
+Required string.  
+The full file path to the output file.  
+The switch itself can be ommitted if this is the second parameter given.  
+Note: any file existing at the given file path will be overwritten.  
+
+### -OutputFormat ["FlatMachines" | "SmartGroupsFlat" | "SmartGroupsGrouped"]
+Optional string from a predefinied set of strings.  
+The structure of the RDG output file.  
+- `FlatMachines` = All machines from the given OU in a single, flat group.
+- `SmartGroupsFlat` = All machines from the given OU in a single flat group, and a smart group for each lab containing at least X computers, given by `-MinLabSizeForSmartGroups`, all in a flat structure.
+- `SmartGroupsGrouped` = A group for each building, which contains A) a smart group for each lab in that building, and B) a flat group of all machines in that building. Not compatible with `-MinLabSizeForSmartGroups`, due to the way RDCMan grouping works.
+- `MirrorOuStructure` = groups in a structure mirroring the given OU structure, with computers where you would expect them. Not implemented yet!
+Default is `SmartGroupsGrouped`.  
+
+### -MinLabSizeForSmartGroups \<int\>
+Optional integer.  
+The minimum number of discovered computers which must share a name prefix (e.g. `MEL-1001-`) for an associated smart group to be created.  
+Default is `2`.  
+The pre-made file above was generated using a value of `3`.  
+
+# Notes
+- For whatever reason, RDCMan has a seemingly arbitrary built-in limitation where the GUI will not allow you to add computers to the same group as other groups or smart groups. However RDCMan _will_ read an RDG file with such a mixed structure without issue (why Microsoft!?). I guess you can't expect too much from a 10+ year old app which has been abandoned for 7 years.
+- An additional limitation (or rather more of a design decision) which makes the code more complex, is that smart groups will only search for matching computers which are _adjacent to_ or _below_ them in the group hierarchy.
+- By mseng3. See my other projects here: https://github.com/mmseng/code-compendium.
